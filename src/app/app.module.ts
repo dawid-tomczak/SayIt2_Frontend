bb136e27d9913256819489a956a30928355c5cc8
Dawid <dawid@tomczakd.pl>
2019-11-19 11:52:07 +0100

    init of background component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BackgroundComponent } from './components/background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
