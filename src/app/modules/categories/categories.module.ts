cd623ad61c654faf34af810e79f43877e2ce96ad
tomczada <dawid.tomczak@dmgmori.com>
2020-12-30 16:42:21 +0100

    SIT-75 categories are now shown, but the cards need to be styled

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesPageComponent } from './components/categories-page/categories-page.component';
 import { HeroHeaderComponent } from './components/hero-header/hero-header.component';
+import { MaterialModule } from 'src/app/shared/modules/material/material.module';
+import { CategoryCardComponent } from './components/category-card/category-card.component';
 
 
 @NgModule({
-  declarations: [CategoriesPageComponent, HeroHeaderComponent],
+  declarations: [CategoriesPageComponent, HeroHeaderComponent, CategoryCardComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    MaterialModule
   ]
 })
 export class CategoriesModule { }
--- src/app/modules/categories/components/categories-page/categories-page.component.ts
+++ src/app/modules/categories/components/categories-page/categories-page.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   templateUrl: './categories-page.component.html',
   styleUrls: ['./categories-page.component.scss']
 })
-export class CategoriesPageComponent implements OnInit {
+export class CategoriesPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  loadingCategories: boolean = false;
+  categories: Category[] = [];
+  private subscriptions: Subscription[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.subscriptions.push(this.downloadCategories());
   }
 
+  private downloadCategories(): Subscription {
+    this.loadingCategories = true;
+
+    return this.categoryService.getCategories().subscribe(res => {
+      this.categories = res;
+      this.loadingCategories = false;
+    }, err => {
+      this.loadingCategories = false;
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
