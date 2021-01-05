9e7a8fd66d89d7a18451c3e0723b1f9f92b1b5aa
dudexior <dawidson1996@gmail.com>
2021-01-05 18:50:33 +0100

    SIT-82 displaying of flashcard desc works now properly on mobile devices

--- src/app/modules/flashcards/components/flashcard/flashcard.component.ts
+++ src/app/modules/flashcards/components/flashcard/flashcard.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { slideHeight } from 'src/app/shared/animations';
+import { MobileDeviceService } from 'src/app/shared/services/mobile-device.service';
 import { Flashcard } from '../../models/flashcard';
 import { FlashcardChangeDirection } from '../../models/flashcard-change-direction.enum';
 import { FlashcardService } from '../../services/flashcard.service';
@@ -10,7 +12,7 @@ import { FlashcardService } from '../../services/flashcard.service';
   styleUrls: ['./flashcard.component.scss'],
   animations: [slideHeight]
 })
-export class FlashcardComponent implements OnInit {
+export class FlashcardComponent implements OnInit, OnDestroy {
 
   @Input() flashcard: Flashcard;
   @Input() index: number;
@@ -20,14 +22,41 @@ export class FlashcardComponent implements OnInit {
   showDescription = false;
   // ref to enum for using in HTML
   changeDirection = FlashcardChangeDirection;
+  $mobileDeviceSubscription: Subscription;
+  displayedOnMobileDevice: boolean;
 
-  constructor(public flashcardService: FlashcardService) { }
+  constructor(public flashcardService: FlashcardService, private mobileService: MobileDeviceService) {
+  }
 
   ngOnInit() {
+    this.startListeningToDeviceType();
   }
 
   descriptionToggle() {
     this.showDescription = !this.showDescription;
+  }
+
+  // need to create a separate function with if because (tap) in html is also triggered on mouse clicks
+  tapHandler() {
+    console.log(this.displayedOnMobileDevice);
+    if (this.displayedOnMobileDevice) {
+      this.descriptionToggle();
+    }
+  }
+
+  private startListeningToDeviceType() {
+    if (!this.$mobileDeviceSubscription) {
+      this.$mobileDeviceSubscription = this.mobileService.getMobileDeviceObservable().subscribe(res => {
+        this.displayedOnMobileDevice = res;
+        console.log(res);
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.$mobileDeviceSubscription) {
+      this.$mobileDeviceSubscription.unsubscribe();
+    }
   }
 
 }
