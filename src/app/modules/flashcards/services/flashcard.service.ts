41b49fd6d8ea347d8fc5d2666cd4abf90a925593
dudexior <dawidson1996@gmail.com>
2021-01-21 20:22:01 +0100

    SIT-119 added service function

--- src/app/modules/flashcards/services/flashcard.service.ts
+++ src/app/modules/flashcards/services/flashcard.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ALL_FLASHCARDS_ENDPOINT } from 'src/app/shared/consts';
+import { ALL_FLASHCARDS_ENDPOINT, FLASHCARD_FROM_CATEGORY } from 'src/app/shared/consts';
 import { Translation } from 'src/app/shared/models/translation';
 import { TranslationService } from 'src/app/shared/services/translation.service';
 import { Flashcard } from '../models/flashcard';
@@ -13,8 +13,14 @@ export class FlashcardService {
 
   constructor(private http: HttpClient, private translationService: TranslationService) { }
 
-  getAllFlashcards(): Observable<any[]> {
+  getAllFlashcards(): Observable<Flashcard[]> {
     const url = ALL_FLASHCARDS_ENDPOINT;
+
+    return this.http.get<Flashcard[]>(url);
+  }
+
+  getFlashcardsFromCategory(categoryId: number): Observable<Flashcard[]> {
+    const url = FLASHCARD_FROM_CATEGORY.replace(/:ID/, categoryId.toString());
 
     return this.http.get<Flashcard[]>(url);
   }
