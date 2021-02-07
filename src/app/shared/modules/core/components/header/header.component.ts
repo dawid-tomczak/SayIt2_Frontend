00220899c1a401c93cea34077b1728bca3c31ccb
Jenkins <jenkins.tomczakd.pl>
2021-02-07 16:24:43 +0100

    [Jenkins] TSLint fixes

--- src/app/shared/modules/core/components/header/header.component.ts
+++ src/app/shared/modules/core/components/header/header.component.ts
@@ -18,7 +18,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   subscriptions: Subscription[] = [];
 
   constructor(private loginService: LoginService, private router: Router,
-    private snackbarService: SnackbarService, private location: Location, private userService: UserService) { }
+              private snackbarService: SnackbarService, private location: Location, private userService: UserService) { }
 
   ngOnInit() {
     this.subscriptions.push(
